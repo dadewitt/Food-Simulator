60677308307c9d5a2ce7f1c8e725aebcf069f2de
dadewitt <dadewitt@purdue.edu>
2014-05-03 19:30:22 -0400

    Added comments to Client; created bin directory
    To run the programs, go into the bin directory and type 'java
    Server/Client_Test' .... Make sure the Server is running before you run
    the Client.

--- Client_Test.java
+++ Client_Test.java
@@ -2,25 +2,37 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client_Test {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the test client. Type in your message to send it to the server, then press enter. To leave, type 'exit'");
 		try {
+			// Open scanner to grab messages from standard input
 			Scanner inp = new Scanner(System.in);
+			
+			// Open socket to connect to the server; first argument is the host, second argument is the port
+			Socket serverCon = new Socket("localhost", 3001);
+			
+			// Store commend
 			String cmd = "";
 			do {
+				// Grabe the message
 				cmd = inp.nextLine();
-				Socket serverCon = new Socket("localhost", 3001);
+				
+				if (cmd.equals("exit"))
+					break;
+				
+				// Open a PrintWriter to be able to write (string) messages to the server
 				java.io.PrintWriter writer = new java.io.PrintWriter(serverCon.getOutputStream(), true);
-				writer.write("CLIENT: " + cmd);
-				System.out.println(cmd);
+				
+				// Write the command to the server's input pipe (server uses readLine, so use writer.println())
+				writer.println("CLIENT: " + cmd);
+				
+				// Send the data to the server
 				writer.flush();
-				serverCon.close();
 				
 			} while (!cmd.equals("exit"));
+			
+			// Close the connection to the server
+			serverCon.close();
 			
 		} catch (Exception e) {}
 	}
--- Server.java
+++ Server.java
@@ -34,33 +34,36 @@ public class Server {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+			int clientId = 0;
 			cmdGet = new CommandThread();
 			cmdGet.start();
 			server = new ServerSocket(3001);
+			System.out.println("Server started at" + server.getLocalSocketAddress() + ":" + server.getLocalPort());
+			System.out.println("To disconnect the server and regain console control, type 'exit'\n");
 			while (!cmdGet.getCommand().equals("exit")) {
 				Socket client = server.accept();
 				java.io.PrintWriter writer = new java.io.PrintWriter(client.getOutputStream(), true);
-				System.out.println("client IP: " + client.getRemoteSocketAddress().toString());
+				System.out.println("Client Connected! ID=" + clientId++);
 				java.io.BufferedReader reader = new java.io.BufferedReader(new InputStreamReader(client.getInputStream()) );
 				String request = "";
-				System.out.println("ready to read: " + reader.ready());
-				while (reader.ready()) {
-					String bob = reader.readLine();
+				//System.out.println("ready to read: " + reader.ready());
+				String bob = "";
+				while ( (bob = reader.readLine()) != null) {
 					System.out.println("client MsG: " + bob);
 					request += bob + "\n";
 				}
-				HeaderDictionary fieldPairs = HttpReader.parseRequest(request);
+				//HeaderDictionary fieldPairs = HttpReader.parseRequest(request);
 				//fieldPairs.print();
-				writer.write("HTTP/1.1 200 Document follows\r\nServer: Test-Server\r\nContent-Type: text/html\r\n\r\n");
-				writer.append("Hello, world!<br /><a href='http://www.google.com'>Google<a>");
-				writer.flush();
+				//writer.write("HTTP/1.1 200 Document follows\r\nServer: Test-Server\r\nContent-Type: text/html\r\n\r\n");
+				//writer.append("Hello, world!<br /><a href='http://www.google.com'>Google<a>");
+				//writer.flush();
 				client.close();
+				System.out.println("Client Disconnected.\n");
 			}
+			
+			server.close();
 				
-		} catch (Exception e) {
-			System.out.println("Something went wrong.");
-			e.printStackTrace();
-		}
+		} catch (Exception e) {		}
 	}
 
 }
